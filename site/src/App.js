abfe6037ef55e3ee6ada0e7818960a4043796668
Kyler <39353605+fisher60@users.noreply.github.com>
2022-01-31 10:53:28 -0600

    Feature/home component (#1)

--- site/src/App.js
+++ site/src/App.js
@@ -1,32 +1,11 @@
-import { Box, Container, Heading, Stack } from "@chakra-ui/react";
-import { useState } from "react";
+import { Home } from "./components/home/Home";
+
 import "./App.css";
-import Switch from "./components/Switch.js";
-import "./components/Switch.css";
+
 
 function App() {
-  const [theme, setTheme] = useState("dark-mode");
-
-  const themeToggler = () => {
-    theme === "dark-mode" ? setTheme("light-mode") : setTheme("dark-mode");
-  };
-
   return (
-    <Container className={theme}>
-      <Box display={{ md: "flex" }} align="center">
-        <Box flexGrow={1}>
-          <Stack spacing={5}>
-            <Heading as="h1" className="homepage-header">
-              Xithrius
-            </Heading>
-            <Heading as="h2" className="homepage-header-description">
-              Rustacean, Pythonista, Triager.
-            </Heading>
-          </Stack>
-          <Switch onToggle={() => themeToggler()} />
-        </Box>
-      </Box>
-    </Container>
+    <Home />
   );
 }
 
