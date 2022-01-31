1f29f53cee30921f8a2b95961613ec989aab5b6f
Xithrius <xithrius@gmail.com>
2022-01-31 05:26:36 -0800

    Added switch to app

--- site/src/App.js
+++ site/src/App.js
@@ -1,16 +1,31 @@
+import Switch from "./components/Switch";
 import "./App.css";
 import { Container, Box, Heading, Stack } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const themeToggler = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
+
   return (
     <Container>
+      <Box display={{ md: "flex" }} align="right">
+        <Box flexGrow={1}>
+          <Switch onToggle={() => themeToggler()} />
+        </Box>
+      </Box>
       <Box display={{ md: "flex" }} align="center">
         <Box flexGrow={1}>
           <Stack spacing={5}>
             <Heading as="h1" className="homepage-header">
               Xithrius
             </Heading>
-            <Heading>Rustacean, Pythonista, Triager.</Heading>
+            <Heading as="h2" className="homepage-header-description">
+              Rustacean, Pythonista, Triager.
+            </Heading>
           </Stack>
         </Box>
       </Box>
