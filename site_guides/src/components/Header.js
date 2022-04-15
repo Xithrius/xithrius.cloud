e145731f32b04dd34ccd53532cd41ff6caa35a73
Xithrius <xithrius@gmail.com>
2022-04-15 14:47:03 -0700

    Fixed home hyperlink underline, removed useless comments

--- site_guides/src/components/Header.js
+++ site_guides/src/components/Header.js
@@ -4,8 +4,10 @@ import styled from "styled-components";
 import { MdNorthEast } from "react-icons/md";
 
 const HeaderStyles = styled.div`
-  text-decoration: underline;
-  text-decoration-color: #ff2e88;
+  -webkit-text-decoration: #ff2e88 solid underline;
+  text-decoration: #ff2e88 solid underline;
+  -webkit-text-underline-position: under;
+  text-underline-position: under;
 `;
 
 const ExternalLink = styled(MdNorthEast)`
