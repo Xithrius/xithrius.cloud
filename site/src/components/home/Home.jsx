ac2d1d67ef45504dc3c32e8274b76c48132cfb72
Xithrius <15021300+Xithrius@users.noreply.github.com>
2022-05-05 15:56:23 -0700

    More icons, more links. (#39)
    Co-authored-by: Xithrius <15021300+Xithrius@users.noreply.github.com>
    Co-authored-by: Kieran Siek <kieransiek@protonmail.com>

--- site/src/components/home/Home.jsx
+++ site/src/components/home/Home.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { Box, Container, Heading, Stack } from "@chakra-ui/react";
+import { SiGithub, SiGitea, SiTwitch, SiYoutube } from "react-icons/si";
+import { IconContext } from "react-icons";
 import Switch from "../switch/Switch";
 import "./Home.css";
+
+const iconStyle = { margin: "4px" };
 
 export function Home() {
   const [theme, setTheme] = useState("dark-mode");
@@ -9,6 +13,7 @@ export function Home() {
   const themeToggler = () => {
     theme === "dark-mode" ? setTheme("light-mode") : setTheme("dark-mode");
   };
+
   return (
     <Container className={theme}>
       <Box display={{ md: "flex" }} align="center">
@@ -22,6 +27,24 @@ export function Home() {
             </Heading>
           </Stack>
           <Switch onToggle={() => themeToggler()} />
+        </Box>
+        <Box display="inline-flex">
+          <IconContext.Provider
+            value={{ className: "shared-class", size: 40, style: iconStyle }}
+          >
+            <a href="https://github.com/Xithrius">
+              <SiGithub />
+            </a>
+            <a href="https://git.xithrius.cloud/">
+              <SiGitea />
+            </a>
+            <a href="https://twitch.tv/Xithrius">
+              <SiTwitch />
+            </a>
+            <a href="https://www.youtube.com/channel/UCD97h_kt9b3dhHPaDpsHevA">
+              <SiYoutube />
+            </a>
+          </IconContext.Provider>
         </Box>
       </Box>
     </Container>
