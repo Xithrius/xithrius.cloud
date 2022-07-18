c9f39ee5e323622e9f1333ebb21a18461e8b70c1
Xithrius <15021300+Xithrius@users.noreply.github.com>
2022-07-17 22:20:53 -0700

    Images, more guides. (#109)
    * Updated dependencies
    
    * Trying to make images work
    
    * Fix static image links. (#111)
    
    Co-authored-by: Kieran Siek <git@siek.dev>

--- site_guides/components/intro.tsx
+++ site_guides/components/intro.tsx
@@ -2,7 +2,7 @@ const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        blog.xithrius.cloud
+        guides.xithrius.cloud
       </h1>
     </section>
   );
