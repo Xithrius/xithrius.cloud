dec38adaed003d192d4c2df4930bceccc46e452d
Xithrius <xithrius@gmail.com>
2022-07-17 23:37:51 -0700

    Removed remark-images because it was useless

--- site_guides/lib/markdownToHtml.ts
+++ site_guides/lib/markdownToHtml.ts
@@ -1,8 +1,7 @@
 import { remark } from "remark";
 import html from "remark-html";
-import remarkImages from "remark-images";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).use(remarkImages).process(markdown);
+  const result = await remark().use(html).process(markdown);
   return result.toString();
 }
