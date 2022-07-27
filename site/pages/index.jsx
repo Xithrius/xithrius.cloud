e78d05564437cd429464a47e0daab51bf34e1ec5
Xithrius <xithrius@gmail.com>
2022-07-27 16:16:32 -0700

    Menu items now have `_blank` targets

--- site/pages/index.jsx
+++ site/pages/index.jsx
@@ -15,10 +15,18 @@ export default function Home() {
         <p className={styles.description}>Rustacean, Pythonista, Triager.</p>
 
         <div className={styles.menu}>
-          <a href="https://guides.xithrius.cloud">Guides</a>
-          <a href="https://todo.xithrius.cloud">ToDo</a>
-          <a href="https://github.com/Xithrius">GitHub</a>
-          <a href="https://twitch.tv/Xithrius">Twitch</a>
+          <a href="https://guides.xithrius.cloud" target="_blank">
+            Guides
+          </a>
+          <a href="https://todo.xithrius.cloud" target="_blank">
+            ToDo
+          </a>
+          <a href="https://github.com/Xithrius" target="_blank">
+            GitHub
+          </a>
+          <a href="https://twitch.tv/Xithrius" target="_blank">
+            Twitch
+          </a>
           <a href="https://www.youtube.com/channel/UCD97h_kt9b3dhHPaDpsHevA">
             YouTube
           </a>
