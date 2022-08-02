ed2b578dbe0a275f522303f437df60dee314e47c
Xithrius <xithrius@gmail.com>
2022-08-01 23:09:10 -0700

    Links are now more intuitive, header changed to `blog.xithrius.cloud`

--- site_blog/components/header.tsx
+++ site_blog/components/header.tsx
@@ -5,10 +5,9 @@ const Header = () => {
     <h2 className="mb-20 mt-8 text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
       <Link href="/">
         <a className="opacity-50 hover:opacity-100">
-          Blog
+          blog.xithrius.cloud
         </a>
       </Link>
-      .
     </h2>
   );
 };
