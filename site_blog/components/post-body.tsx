c6378f2fc1eeb2f57344d1078110a01e1ee779ac
Xithrius <xithrius@gmail.com>
2022-08-08 04:20:28 -0700

    Something somewhere is conflicting, I can't find it

--- site_blog/components/post-body.tsx
+++ site_blog/components/post-body.tsx
@@ -5,7 +5,15 @@ type Props = {
 const PostBody = ({ content }: Props) => {
   return (
     <div
-      className="prose prose-sm mx-auto max-w-2xl prose-code:before:content-none prose-code:after:content-none dark:prose-invert lg:prose-base"
+      className="
+      prose
+      prose-dark
+      mx-auto
+      max-w-2xl
+      prose-code:before:content-none
+      prose-code:after:content-none
+      dark:prose-invert
+      "
       dangerouslySetInnerHTML={{ __html: content }}
     />
   );
