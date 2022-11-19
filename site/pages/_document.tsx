47936d21318dd5c3b414c74c637c8983e9a20226
kosayoda <git@siek.dev>
2022-11-18 19:37:33 -0500

    Fix theme toggle.

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body className="bg-light-bg text-light-fg dark:bg-dark-bg dark:text-dark-fg">
+        <body>
           <Main />
           <NextScript />
         </body>
