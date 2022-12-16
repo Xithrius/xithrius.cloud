a1592324c247ece508ddaef3b6e875a25c1bed57
Xithrius <xithrius@gmail.com>
2022-12-15 16:01:08 -0800

    Aligned the blog title a little better

--- site/lib/components/intro.tsx
+++ site/lib/components/intro.tsx
@@ -1,7 +1,7 @@
 const Intro = () => {
   return (
-    <section className="flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
-      <h1 className="text-6xl font-bold leading-tight">
+    <section className="flex flex-col items-center md:mb-6 md:flex-row md:justify-between">
+      <h1 className="text-6xl font-bold leading-tight pl-3">
         Blog
       </h1>
     </section>
