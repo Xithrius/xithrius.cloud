789e0978ba019472fabd433ea571ac0592ac74f9
Xithrius <xithrius@fastmail.com>
2025-07-01 05:31:00 -0700

    Don't center blog titles when on non-mobile screens

---
+++ TODO.md
@@ -0,0 +1,3 @@
+https://github.com/danielcgilibert/blog-template
+https://github.com/markhorn-dev/astro-sphere
+https://github.com/markhorn-dev/astro-nano
--- astro.config.mjs
+++ astro.config.mjs
@@ -8,3 +8,30 @@ export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
 });
+
+// export default defineConfig({
+// 	site: siteConfig.site,
+// 	markdown: {
+// 		remarkPlugins: [remarkReadingTime],
+// 		drafts: true,
+// 		shikiConfig: {
+// 			theme: 'material-theme-palenight',
+// 			wrap: true
+// 		}
+// 	},
+// 	integrations: [
+// 		mdx({
+// 			syntaxHighlight: 'shiki',
+// 			shikiConfig: {
+// 				experimentalThemes: {
+// 					light: 'vitesse-light',
+// 					dark: 'material-theme-palenight',
+// 				  },
+// 				wrap: true
+// 			},
+// 			drafts: true
+// 		}),
+// 		sitemap(),
+// 		tailwind()
+// 	]
+// })
